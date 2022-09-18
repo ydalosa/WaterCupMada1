8ab4e596a534bba85d1280f0728580b7ae685016
Andy <96415472+ydalosa@users.noreply.github.com>
2022-09-18 09:06:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,35 @@ menuBtn.onclick = function(){
         menu.src = "menu4.jpg";
     }
 }
+
+
+// :::::::::::::
+
+const words =["Vita soa amantsara Présentation officielle APR",""," Roso ny dia ","Ho an'i Tanindrazana","Hiroso amin'i fanentanana tsika","Ndao"]
+let cursor = gsap.to('.cursor', {opacity: 0, ease:"power2.inOut", repeat:-1})
+
+let boxTl = gsap.timeline()
+
+boxTl.to('.box', {duration: 1, width:"17vw", delay: 0.5, ease: "power4.inOut"})
+.from('.hi', {duration: 1, y:"7vw", ease: "power3.out", onComplete: () => masterTl.play()})
+.to('.box', {duration: 1, height:"7vw", ease: "elastic.out"})
+.to('.box', {duration: 2, autoAlpha:0.5, yoyo: true, repeat: -1})
+
+let masterTl = gsap.timeline({repeat: -1}).pause()
+
+words.forEach(word => {
+    let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
+    tl.to('.text', {duration: 1, text: word})
+    masterTl.add(tl)
+});
+
+
+// ::::::::
+function bgChanger() {
+    if(this.scrollY > this.innerHeight / 1.3 ){
+        document.body.classList.add("bg-active");
+    } else {
+         document.body.classList.remove("bg-active");
+    }
+   }
+   window.addEventListener("scroll", bgChanger);
